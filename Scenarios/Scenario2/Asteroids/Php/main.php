f663a1d677c1781a947e15138f85937683b2ea2a
Tomas Husak <tomikhusak@seznam.com>
2021-02-17 16:41:42 +0100

    Clean Scenario 2

--- Scenarios/Scenario2/Asteroids/Php/main.php
+++ Scenarios/Scenario2/Asteroids/Php/main.php
@@ -11,35 +11,11 @@ class AsteroidsComponent extends \Microsoft\AspNetCore\Components\ComponentBase
 
 	public function BuildRenderTree($builder) : void 
 	{
-		$builder->OpenElement(0,"button");
-		//\Microsoft\AspNetCore\Components\EventCallback::Factory->Create($this, function() {});
-		//$this->AddAttribute(1,"onclick", \Microsoft\AspNetCore\Components\EventCallback::Factory::Create($this, ClickHander));
-		$builder->AddContent(2,"Click");
-		$builder->CloseElement();
-
-		//$this->app->writeWithTreeBuilder($builder, 0);
+		$this->app->writeWithTreeBuilder($builder, 0);
 	}
 
 	public function OnInitialized() : void 
 	{
-		$foo = \Microsoft\AspNetCore\Components\EventCallback;
-		/*$class = new \ReflectionClass($foo);
-
-
-		$methods = $class->getMethods();
-		foreach($methods as $method)
-		{
-			\System\Console::WriteLine($method->getName());
-		}
-		*/
-		$reflect = new \ReflectionClass($foo);
-		$props   = $reflect->getProperties(\ReflectionProperty::IS_PUBLIC);
-
-		foreach ($props as $prop) {
-			\System\Console::WriteLine($prop->getName());
-		}
-
-
 		require(__DIR__ . "/settings.php"); // once !!!
 		$this->app = new Application($settings);		
 	}
