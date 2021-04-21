144143d73eb55986a1653caf724f375a53433956
Tomas Husak <49864539+TomatorCZ@users.noreply.github.com>
2021-04-21 10:37:32 +0200

    Add Benchmark gd

---
+++ Benchmark/GD/Scripts/index.php
@@ -0,0 +1,7 @@
+﻿<?php
+
+echo "<p>GD2 started at" . date("h:i:sa") . "</p>";
+$redimg = imagecreatetruecolor(1920, 1080);
+if ($redimg)
+	echo "<p>Success</p>";
+echo "<p>GD2 ended at" . date("h:i:sa") . "</p>";
