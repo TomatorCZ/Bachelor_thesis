61cf027b6772f991351835b68a3b8fdf00de331a
Tomas Husak <49864539+TomatorCZ@users.noreply.github.com>
2021-04-27 17:13:25 +0200

    The AllTogether section

--- Demo/AllTogether/PHPScripts/Web/index.php
+++ Demo/AllTogether/PHPScripts/Web/index.php
@@ -5,8 +5,8 @@
 
 <h1>Index</h1>
 
-<h3>You can play a web game written in PHP</h3>
-<p><a href="/Graph/index.php">With Score</a></p>
+<h3>You can play a web game written in PHP.</h3>
+<p><a href="/Graph/index.php">Start</a></p>
 
 <h3>Easy Configuration</h3>
 <p>This static web was configured just by 2 lines of code.</p>
@@ -20,7 +20,8 @@ public static async Task Main(string[] args)
 
         // Add PHP
         <b>builder.AddPhp(new[] { typeof(force).Assembly });</b>
-        // builder.RootComponents.Add(typeof(PhpScriptProvider), "#app", ParameterView.FromDictionary(new Dictionary<string, object>() { { "ContextLifetime", SessionLifetime.Persistant} }));
+        // builder.RootComponents.Add(typeof(PhpScriptProvider), "#app",
+        //ParameterView.FromDictionary(new Dictionary<string, object>() { { "ContextLifetime", SessionLifetime.Persistant} }));
         <b>builder.RootComponents.Add(typeof(PhpScriptProvider), "#app");</b>
             
         await builder.Build().RunAsync();
