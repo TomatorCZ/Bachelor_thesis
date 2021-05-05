cb537f4f685be0f60b05a9b14c64e856d454ce90
Tomas Husak <49864539+TomatorCZ@users.noreply.github.com>
2021-05-05 16:49:35 +0200

    Peachpie.Blazor Razor library

---
+++ src/Peachpie.Blazor/Peachpie.Blazor/Server/IApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peachpie.Blazor
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static void UseAdditionalWebStaticAssets(this IApplicationBuilder app, IConfiguration config)
+        {
+            foreach (var item in config.GetSection("AdditionalWebStaticAssets").GetChildren())
+            {
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(item["Path"]),
+                    RequestPath = item["BasePath"]
+                });
+            }
+        }
+    }
+}
