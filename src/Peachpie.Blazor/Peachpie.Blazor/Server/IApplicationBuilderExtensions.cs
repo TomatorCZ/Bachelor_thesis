ba67a34344ea5b8d84a8f7a77bd2642acfdf5dfb
Tomas Husak <49864539+TomatorCZ@users.noreply.github.com>
2021-05-10 22:22:05 +0200

    Add support for relative paths

--- src/Peachpie.Blazor/Peachpie.Blazor/Server/IApplicationBuilderExtensions.cs
+++ src/Peachpie.Blazor/Peachpie.Blazor/Server/IApplicationBuilderExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.FileProviders;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,13 @@ namespace Peachpie.Blazor
         {
             foreach (var item in config.GetSection("AdditionalStaticWebAssets").GetChildren())
             {
+                string path = item["Path"];
+                if (!Path.IsPathRooted(item["Path"]))
+                    path = Path.Combine(Directory.GetCurrentDirectory() + "\\", item["Path"]);
+
                 app.UseStaticFiles(new StaticFileOptions
                 {
-                    FileProvider = new PhysicalFileProvider(item["Path"]),
+                    FileProvider = new PhysicalFileProvider(path),
                     RequestPath = item["BasePath"]
                 });
             }
